6121b8c1180da5c41404d169505da209f6eddd12
kujatic <kukuhsudrajad354@gmail.com>
2025-08-21 20:33:50 +0700

    some soil parameters

--- src/main/java/com/tbear9/plants_api2/Controller.java
+++ src/main/java/com/tbear9/plants_api2/Controller.java
@@ -1,8 +1,10 @@
 package com.tbear9.plants_api2;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    static ObjectMapper mapper = new ObjectMapper();
 
 }
